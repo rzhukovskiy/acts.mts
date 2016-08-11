1c0781ac19c4b96ddc557330e45377b684ed90e2
hello.omny <hello.omny@gmail.com>
2016-08-11 13:22:42 +0300

    fix date columns, remove db type verification, comment FK, add Yii2 dropTable

--- console/migrations/m160809_135225_cards_added.php
+++ console/migrations/m160809_135225_cards_added.php
@@ -1,42 +1,37 @@
 <?php
 
-use yii\db\Migration;
+    use yii\db\Migration;
 
-class m160809_135225_cards_added extends Migration
-{
-    public function up()
+    class m160809_135225_cards_added extends Migration
     {
-        $tables = Yii::$app->db->schema->getTableNames();
-        $dbType = $this->db->driverName;
-        $tableOptions_mysql = "CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB";
+        public function up()
+        {
+            $tableOptions_mysql = "CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB";
 
-        if (!in_array('acts_card', $tables))  {
-            if ($dbType == "mysql") {
-                $this->createTable('{{%card}}', [
-                    'id' => 'INT(10) UNSIGNED NOT NULL AUTO_INCREMENT',
-                    0 => 'PRIMARY KEY (`id`)',
-                    'company_id' => 'INT(10) UNSIGNED NOT NULL',
-                    'number' => 'INT(11) NOT NULL',
-                    'status' => 'SMALLINT(6) NOT NULL DEFAULT \'10\'',
-                    'created_at' => 'INT(11) NOT NULL',
-                    'updated_at' => 'INT(11) NOT NULL',
-                ], $tableOptions_mysql);
-            }
+            $this->createTable( '{{%card}}', [
+                'id' => 'INT(10) UNSIGNED NOT NULL AUTO_INCREMENT',
+                0 => 'PRIMARY KEY (`id`)',
+                'company_id' => 'INT(10) UNSIGNED NOT NULL',
+                'number' => 'INT(11) NOT NULL',
+                'status' => 'SMALLINT(6) NOT NULL DEFAULT \'10\'',
+                'created_at' => 'DATETIME NOT NULL',
+                'updated_at' => 'TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP',
+            ], $tableOptions_mysql );
+
+
+            $this->createIndex( 'idx_UNIQUE_number_4304_00', '{{%card}}', 'number', 1 );
+            $this->createIndex( 'idx_company_id_4304_01', '{{%card}}', 'company_id', 0 );
+
+            $this->execute( 'SET foreign_key_checks = 0' );
+            // TODO: maybe RESTRICT on delete
+            //$this->addForeignKey( 'fk_acts_company_4295_00', '{{%card}}', 'company_id', '{{%card}}', 'id', 'CASCADE', 'CASCADE' );
+            $this->execute( 'SET foreign_key_checks = 1;' );
         }
 
-
-        $this->createIndex('idx_UNIQUE_number_4304_00','{{%card}}','number',1);
-        $this->createIndex('idx_company_id_4304_01','{{%card}}','company_id',0);
-
-        $this->execute('SET foreign_key_checks = 0');
-        $this->addForeignKey('fk_acts_company_4295_00','{{%card}}', 'company_id', '{{%card}}', 'id', 'CASCADE', 'CASCADE' );
-        $this->execute('SET foreign_key_checks = 1;');
+        public function down()
+        {
+            $this->execute( 'SET foreign_key_checks = 0' );
+            $this->dropTable('{{$card}}');
+            $this->execute( 'SET foreign_key_checks = 1;' );
+        }
     }
-
-    public function down()
-    {
-        $this->execute('SET foreign_key_checks = 0');
-        $this->execute('DROP TABLE IF EXISTS `acts_card`');
-        $this->execute('SET foreign_key_checks = 1;');
-    }
-}
