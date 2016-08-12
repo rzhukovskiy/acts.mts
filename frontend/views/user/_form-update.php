b321082b837914cb2de7d0cbcd45d4c3fdf7c1b1
hello.omny <hello.omny@gmail.com>
2016-08-12 18:19:13 +0300

    layout and style

--- frontend/views/user/_form-update.php
+++ frontend/views/user/_form-update.php
@@ -1,17 +1,24 @@
 <?php
 
+use yii\bootstrap\ActiveForm;
 use yii\helpers\Html;
-use yii\widgets\ActiveForm;
 
 /**
  * @var $this yii\web\View
  * @var $model common\models\User
  * @var $form yii\widgets\ActiveForm
  */
+
 ?>
-<div class="user-form">
+<div class="user-update-form">
     <?php
-    $form = ActiveForm::begin();
+    $form = ActiveForm::begin([
+        'options' => ['class' => 'form-horizontal col-sm-12'],
+        'fieldConfig' => [
+            'template' => '{label}<div class="col-sm-6 input-sm">{input}{error}</div>',
+            'labelOptions' => ['class' => 'col-sm-2 control-label'],
+        ],
+    ]);
     echo $form->field($model, 'username')->textInput();
     // ToDo: нужно вообще менять пароль именно тут? Или пользователи сами могут это сделать
     echo $form->field($model, 'newPassword')->passwordInput();
@@ -26,7 +33,8 @@ use yii\widgets\ActiveForm;
         );
     ?>
     <div class="form-group">
-        <?= Html::submitButton('Изменить', ['class' => 'btn btn-primary']) ?>
+        <div class="col-sm-6 col-sm-offset-2"><?= Html::submitButton('Изменить', ['class' => 'btn btn-primary']) ?>
+        </div>
     </div>
     <?php ActiveForm::end(); ?>
 </div>
--- frontend/views/user/update.php
+++ frontend/views/user/update.php
@@ -4,14 +4,19 @@ use yii\bootstrap\Html;
 /**
  * @var $this \yii\web\View
  */
-$this->title = 'Пользователи';
+$this->title = 'Редактирование пользователя ' . Html::encode($model->username);
 $this->params['breadcrumbs'][] = $this->title;
 
 ?>
 <div class="user-update">
     <h1><?= Html::encode($this->title) ?></h1>
-    <?= $this->render('_form-update', [
-        'model' => $model,
-        'companyDropDownData' => $companyDropDownData,
-    ]) ?>
+    <div class="panel panel-primary">
+        <div class="panel-heading">Редактировать пользователя</div>
+        <div class="panel-body">
+            <?= $this->render('_form-update', [
+                'model' => $model,
+                'companyDropDownData' => $companyDropDownData,
+            ]) ?>
+        </div>
+    </div>
 </div>
