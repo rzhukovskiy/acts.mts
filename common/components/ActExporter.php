0ba2c2f0fcae7cdb2c492a6fed6f75d1c44141b5
rzhukovskiy <zhukovskey@gmail.com>
2016-08-31 18:05:22 +0300

    disinfect acts export fixed

--- common/components/ActExporter.php
+++ common/components/ActExporter.php
@@ -1921,7 +1921,7 @@ class ActExporter
             $cnt++;
             if ($cnt == 5) {
                 $cnt = 0;
-                $startRow += mb_strlen($company->name) > 55 ? 26 : 28;
+                $startRow += mb_strlen($company->name) > 55 ? 25 : 28;
             }
         }
     }
