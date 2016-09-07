196d39adba4302e5d0e3f4b3867fcfeabfc0f3f4
hello.omny <hello.omny@gmail.com>
2016-09-08 00:03:29 +0300

    custom date range widget for other users

--- frontend/views/stat/view/partner.php
+++ frontend/views/stat/view/partner.php
@@ -1,6 +1,6 @@
 <?php
 
-use yii\grid\GridView;
+use kartik\grid\GridView;
 use yii\bootstrap\Html;
 use common\components\DateHelper;
 use common\assets\CanvasJs\CanvasJsAsset;
@@ -14,11 +14,87 @@ use common\assets\CanvasJs\CanvasJsAsset;
 
 CanvasJsAsset::register($this);
 
-echo $this->render('../_search', [
-    'type' => null,
-    'companyId' => $model->id,
-    'model' => $searchModel,
-]);
+/**
+ * Виджет выбора диапазона дат
+ */
+$halfs = [
+    '1е полугодие',
+    '2е полугодие'
+];
+$quarters = [
+    '1й квартал',
+    '2й квартал',
+    '3й квартал',
+    '4й квартал',
+];
+$months = [
+    'январь',
+    'февраль',
+    'март',
+    'апрель',
+    'май',
+    'июнь',
+    'июль',
+    'август',
+    'сентябрь',
+    'октябрь',
+    'ноябрь',
+    'декабрь',
+];
+
+$ts1 = strtotime($searchModel->dateFrom);
+$ts2 = strtotime($searchModel->dateTo);
+
+$year1 = date('Y', $ts1);
+$year2 = date('Y', $ts2);
+
+$month1 = date('m', $ts1);
+$month2 = date('m', $ts2);
+
+$diff = (($year2 - $year1) * 12) + ($month2 - $month1);
+switch ($diff) {
+    case 1:
+        $period = 1;
+        break;
+    case 3:
+        $period = 2;
+        break;
+    case 6:
+        $period = 3;
+        break;
+    case 12:
+        $period = 4;
+        break;
+    default:
+        $period = 0;
+}
+
+$periodForm = '';
+$periodForm .= Html::dropDownList('period', $period, \common\models\Act::$periodList, ['class' =>'select-period form-control', 'style' => 'margin-right: 10px;']);
+$periodForm .= Html::dropDownList('month', '', $months, ['id' => 'month', 'class' => 'autoinput form-control', 'style' => $diff == 1 ? '' : 'display:none']);
+$periodForm .= Html::dropDownList('half', '', $halfs, ['id' => 'half', 'class' => 'autoinput form-control', 'style' => $diff == 6 ? '' : 'display:none']);
+$periodForm .= Html::dropDownList('quarter', '', $quarters, ['id' => 'quarter', 'class' => 'autoinput form-control', 'style' => $diff == 3 ? '' : 'display:none']);
+$periodForm .= Html::dropDownList('year', 10, range(date('Y') - 10, date('Y')), ['id' => 'year', 'class' => 'autoinput form-control', 'style' => $diff && $diff <= 12 ? '' : 'display:none']);
+$periodForm .= Html::activeTextInput($searchModel, 'dateFrom', ['class' => 'date-from ext-filter hidden']);
+$periodForm .= Html::activeTextInput($searchModel, 'dateTo',  ['class' => 'date-to ext-filter hidden']);
+$periodForm .= Html::submitButton('Показать', ['class' => 'btn btn-primary date-send', 'style' => 'margin-left: 10px;']);
+
+if ($admin) {
+    $filters = 'Выбор компании: ' . Html::activeDropDownList($searchModel, 'client_id', Company::find()->active()
+            ->andWhere(['type' => Company::TYPE_OWNER])
+            ->select(['name', 'id'])->indexBy('id')->column(), ['prompt' => 'все','class' => 'form-control ext-filter', 'style' => 'width: 200px; margin-right: 10px']);
+} elseif (!empty(Yii::$app->user->identity->company->children)) {
+    $filters = 'Выбор филиала: ' . Html::activeDropDownList($searchModel, 'client_id', Company::find()->active()
+            ->andWhere(['parent_id' => Yii::$app->user->identity->company_id])
+            ->select(['name', 'id'])->indexBy('id')->column(), ['prompt' => 'все','class' => 'form-control ext-filter', 'style' => 'width: 200px; margin-right: 10px']);
+}
+
+$filters .= 'Выбор периода: ' . $periodForm;
+
+/**
+ * Конец виджета
+ */
+
 ?>
 <div class="panel panel-primary">
     <div class="panel-heading">
@@ -31,7 +107,38 @@ echo $this->render('../_search', [
             'dataProvider' => $dataProvider,
             'layout' => '{items}',
             'emptyText' => '',
+
             'showFooter' => true,
+            'floatHeader' => $admin,
+            'floatHeaderOptions' => ['top' => '0'],
+            'hover' => false,
+            'striped' => false,
+            'export' => false,
+            'summary' => false,
+            'filterSelector' => '.ext-filter',
+            'beforeHeader' => [
+                [
+                    'columns' => [
+                        [
+                            'content' => $filters,
+                            'options' => ['colspan' => 6, 'style' => 'vertical-align: middle', 'class' => 'kv-grid-group-filter period-select'],
+                        ],
+                    ],
+                    'options' => ['class' => 'filters extend-header'],
+                ],
+                [
+                    'columns' => [
+                        [
+                            'content' => '&nbsp',
+                            'options' => [
+                                'colspan' => 7,
+                            ]
+                        ]
+                    ],
+                    'options' => ['class' => 'kv-group-header'],
+                ],
+            ],
+
             'columns' => [
                 [
                     'header' => '№',
