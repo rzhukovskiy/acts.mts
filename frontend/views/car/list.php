900e50a2ec80d2dd5c7c4128038bea3cbbc47299
rzhukovskiy <zhukovskey@gmail.com>
2017-03-20 18:57:23 +0300

    car list fixed

--- frontend/views/car/list.php
+++ frontend/views/car/list.php
@@ -164,7 +164,7 @@ echo GridView::widget([
         [
             'attribute' => 'car.is_infected',
             'content' => function ($data) {
-                return $data->car->is_infected ? 'да' : 'нет';
+                return !empty($data->car->is_infected) ? 'да' : 'нет';
             },
             'visible' => $admin,
         ],
