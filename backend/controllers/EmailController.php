1c11807cd9983648f606cbb9c241b52d40db4919
artem <artem.mtransservice@mail.ru>
2017-06-28 11:47:26 +0300

    Настройка почтовых шаблонов

--- backend/controllers/EmailController.php
+++ backend/controllers/EmailController.php
@@ -67,8 +67,7 @@ class EmailController extends Controller
         $headers  = 'From: info@mtransservice.ru' . "\r\n";
         $headers .= 'MIME-Version: 1.0' . "\r\n";
 
-        $filename = 'http://offer.mtransservice.ru/files/email/2/%D0%A1%D0%B2%D0%B8%D0%B4%D0%B5%D1%82%D0%B5%D0%BB%D1%8C%D1%81%D1%82%D0%B2%D0%B0%20%D0%BD%D0%B0%20%D0%BC%D0%BE%D0%B9%D0%BA%D1%83.pdf';
-
+        $filename = \Yii::getAlias('@webroot/files/email/2/Свидетельства на мойку.pdf');
 
         $headers .= "Content-Type:multipart/mixed;";
         $headers .= "boundary=\"----------".$un."\"\r\n";
