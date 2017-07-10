d887b5d931fd19e6c200773ae4fbb2431acff285
artem <artem.mtransservice@mail.ru>
2017-07-10 10:48:18 +0300

    Фикс фильтра

--- backend/views/monthly-act/archive/list.php
+++ backend/views/monthly-act/archive/list.php
@@ -4,6 +4,7 @@ use common\assets\CanvasJs\CanvasJsAsset;
 use common\models\Company;
 use yii\bootstrap\Html;
 use kartik\grid\GridView;
+use common\models\DepartmentCompany;
 
 /**
  * @var $this yii\web\View
@@ -20,11 +21,98 @@ use kartik\grid\GridView;
 
 $this->title = "Архив актов";
 
-$filters = \frontend\widgets\datePeriod\DatePeriodWidget::widget([
-    'model'        => $searchModel,
-    'dateFromAttr' => 'dateFrom',
-    'dateToAttr'   => 'dateTo',
+$halfs = [
+    '1е полугодие',
+    '2е полугодие'
+];
+$quarters = [
+    '1й квартал',
+    '2й квартал',
+    '3й квартал',
+    '4й квартал',
+];
+$months = [
+    'январь',
+    'февраль',
+    'март',
+    'апрель',
+    'май',
+    'июнь',
+    'июль',
+    'август',
+    'сентябрь',
+    'октябрь',
+    'ноябрь',
+    'декабрь',
+];
+
+$ts1 = strtotime($searchModel->dateFrom);
+$ts2 = strtotime($searchModel->dateTo);
+
+$year1 = date('Y', $ts1);
+$year2 = date('Y', $ts2);
+
+$month1 = date('m', $ts1);
+$month2 = date('m', $ts2);
+
+$diff = (($year2 - $year1) * 12) + ($month2 - $month1);
+switch ($diff) {
+    case 1:
+        $period = 1;
+        break;
+    case 3:
+        $period = 2;
+        break;
+    case 6:
+        $period = 3;
+        break;
+    case 12:
+        $period = 4;
+        break;
+    default:
+        $period = 0;
+}
+$rangeYear = range(date('Y') - 10, date('Y'));
+$currentYear = isset($searchModel->dateFrom)
+    ? date('Y', strtotime($searchModel->dateFrom))
+    : date('Y');
+
+$currentMonth = isset($searchModel->dateFrom)
+    ? date('n', strtotime($searchModel->dateFrom))
+    : date('n');
+$currentMonth--;
+
+$filters = '';
+$periodForm = '';
+$periodForm .= Html::dropDownList('period', $period, DepartmentCompany::$periodList, [
+    'class' => 'select-period form-control',
+    'style' => 'margin-right: 10px;'
 ]);
+$periodForm .= Html::dropDownList('month', $currentMonth, $months, [
+    'id' => 'month',
+    'class' => 'autoinput form-control',
+    'style' => $diff == 1 ? '' : 'display:none'
+]);
+$periodForm .= Html::dropDownList('half', $currentMonth < 5 ? 0 : 1, $halfs, [
+    'id' => 'half',
+    'class' => 'autoinput form-control',
+    'style' => $diff == 6 ? '' : 'display:none'
+]);
+$periodForm .= Html::dropDownList('quarter', floor($currentMonth / 3), $quarters, [
+    'id' => 'quarter',
+    'class' => 'autoinput form-control',
+    'style' => $diff == 3 ? '' : 'display:none'
+]);
+$periodForm .= Html::dropDownList('year', array_search($currentYear, $rangeYear), range(date('Y') - 10, date('Y')), [
+    'id' => 'year',
+    'class' => 'autoinput form-control',
+    'style' => $diff && $diff <= 12 ? '' : 'display:none'
+]);
+$periodForm .= Html::activeTextInput($searchModel, 'dateFrom', ['class' => 'date-from ext-filter hidden']);
+$periodForm .= Html::activeTextInput($searchModel, 'dateTo', ['class' => 'date-to ext-filter hidden']);
+$periodForm .= Html::submitButton('Показать', ['class' => 'btn btn-primary date-send', 'style' => 'margin-left: 10px;']);
+
+$filters = 'Выбор периода: ' . $periodForm;
 /**
  * Конец виджета
  */
@@ -146,6 +234,10 @@ if($searchModel->client_id) {
 }
 
 if(!$searchModel->client_id) {
+
+    $GLOBALS['dateFrom'] = $searchModel->dateFrom;
+    $GLOBALS['dateTo'] = $searchModel->dateTo;
+
     $columns[] = [
         'label'     => '',
         'contentOptions' => ['style' => 'width: 70px', 'align' => 'center'],
@@ -154,7 +246,9 @@ if(!$searchModel->client_id) {
                 \yii\helpers\Url::to([
                     '/monthly-act/archive',
                     'type'                        => $type,
-                    'MonthlyActSearch[client_id]' => $data->client_id
+                    'MonthlyActSearch[client_id]' => $data->client_id,
+                    'MonthlyActSearch[dateFrom]' => $GLOBALS['dateFrom'],
+                    'MonthlyActSearch[dateTo]' => $GLOBALS['dateTo']
                 ]));
         },
         'format'    => 'raw',
