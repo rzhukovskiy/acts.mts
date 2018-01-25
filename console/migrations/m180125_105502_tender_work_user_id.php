edfc14098b45b37ea964540c168bec3b2464d3ef
artem <artem.mtransservice@mail.ru>
2018-01-25 15:59:52 +0300

    Добавление сотрудника разрабатывающего тех. задание в тендерах

--- console/migrations/m180125_105502_tender_work_user_id.php
+++ console/migrations/m180125_105502_tender_work_user_id.php
@@ -6,12 +6,13 @@ class m180125_105502_tender_work_user_id extends Migration
 {
     public function up()
     {
-        $this->dropColumn('{{%tender}}','work_user_id');
-        $this->dropColumn('{{%tender}}','work_user_time');
+        $this->addColumn('{{%tender}}','work_user_id', 'int(11) DEFAULT 0');
+        $this->addColumn('{{%tender}}','work_user_time', 'varchar(20) DEFAULT NULL');
     }
 
     public function down()
     {
-
+        $this->dropColumn('{{%tender}}','work_user_id');
+        $this->dropColumn('{{%tender}}','work_user_time');
     }
 }
