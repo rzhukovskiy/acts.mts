99bc105e073feaa5e7ea1a91ec2f59b4927087ff
artem <artem.mtransservice@mail.ru>
2018-02-02 12:31:58 +0300

    Фикс меню для Ануш

--- common/models/User.php
+++ common/models/User.php
@@ -292,7 +292,7 @@ class User extends ActiveRecord implements IdentityInterface
 
         // Ануш и Рита по умолчанию вход в шиномонтаж
         if($this->id == 222) {
-            return 4;
+            return 2;
         } else if($this->id == 364) {
             return 4;
         } else {
