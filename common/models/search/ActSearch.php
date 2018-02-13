75f35a3717d61aba400818fd6f48b0732b5fc5ff
Artem Denisov <wolf6806@yandex.ru>
2018-02-13 19:03:10 +0300

    фикс

--- common/models/search/ActSearch.php
+++ common/models/search/ActSearch.php
@@ -373,7 +373,7 @@ class ActSearch extends Act
                         }
                     } else {
                         if (Yii::$app->user->identity->role != User::ROLE_ADMIN) {
-                            $exists = Act::find()->innerJoin('department_linking', 'department_linking.company_id = act.client_id')->where(['AND', ['act.service_type' => $this->service_type], ['act_error.error_type' => 21], ['DATE_FORMAT(FROM_UNIXTIME(`served_at`), "%c-%Y")' => $this->period]])->andWhere(['AND', ['department_linking.user_id' => Yii::$app->user->identity->id], ['department_linking.type' => $this->service_type]])->exists();
+                            $exists = Act::find()->innerJoin('department_linking', 'department_linking.company_id = act.client_id')->where(['AND', ['act.service_type' => $this->service_type], ['DATE_FORMAT(FROM_UNIXTIME(`served_at`), "%c-%Y")' => $this->period]])->andWhere(['AND', ['department_linking.user_id' => Yii::$app->user->identity->id], ['department_linking.type' => $this->service_type]])->exists();
                             if ($exists) {
                                 $this->user_id = Yii::$app->user->identity->id;
                                 $query->innerJoin('department_linking', 'department_linking.company_id = act.client_id');
@@ -425,7 +425,7 @@ class ActSearch extends Act
                         }
                     } else {
                         if (Yii::$app->user->identity->role != User::ROLE_ADMIN) {
-                            $exists = Act::find()->innerJoin('department_linking', 'department_linking.company_id = act.partner_id')->where(['AND', ['act.service_type' => $this->service_type], ['act_error.error_type' => 21], ['DATE_FORMAT(FROM_UNIXTIME(`served_at`), "%c-%Y")' => $this->period]])->andWhere(['AND', ['department_linking.user_id' => Yii::$app->user->identity->id], ['department_linking.type' => $this->service_type]])->exists();
+                            $exists = Act::find()->innerJoin('department_linking', 'department_linking.company_id = act.partner_id')->where(['AND', ['act.service_type' => $this->service_type], ['DATE_FORMAT(FROM_UNIXTIME(`served_at`), "%c-%Y")' => $this->period]])->andWhere(['AND', ['department_linking.user_id' => Yii::$app->user->identity->id], ['department_linking.type' => $this->service_type]])->exists();
                             if ($exists) {
                                 $this->user_id = Yii::$app->user->identity->id;
                                 $query->innerJoin('department_linking', 'department_linking.company_id = act.partner_id');
